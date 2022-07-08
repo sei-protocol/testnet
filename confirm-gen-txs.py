5e2fe35035ee27e348d38ac8451b3620bb46d4f8
philipsu522 <philip.su.522@gmail.com>
2022-07-07 22:07:34 -0700

    Fix gentx scripts

--- confirm-gen-txs.py
+++ confirm-gen-txs.py
@@ -6,30 +6,31 @@ gentx_files = os.listdir('gentx')
 
 invalids = ""
 
-with open('gentx-output.txt', 'w') as outfile:
+with open('gentx-output.csv', 'w') as outfile:
     outfile.write("") # clean file
 
 for file in gentx_files:
+    print(f'Extracting info from {file}')
     f = open('gentx/' + file, 'r')
     data = json.load(f)
 
     validatorData = data['body']['messages'][0]
     moniker = validatorData['description']['moniker']
     rate = float(validatorData['commission']['rate']) * 100
-    valop = validatorData['validator_address']
+    delegator_addr = validatorData['delegator_address']
+    validator_addr = validatorData['validator_address']
     exp = validatorData['value']
-
+### Basic validation ###
     if exp['denom'] != 'usei':
         invalids += f'[!] Invalid denomination for validator: {moniker} {exp["denom"]} \n'
 
     if int(exp['amount'] ) /10000000 != 1.0:
         invalids += f'[!] Invalid amount for validator: {moniker} {int(exp["amount"] ) /10000000}\n'
 
-    with open('gentx-output.txt', 'a') as outfile:
-        outfile.write(f"{valop} {rate}% {moniker}\n")
-    # print(f"{valop} {rate}% {moniker}")
+    with open('gentx-output.csv', 'a') as outfile:
+        outfile.write(f"{delegator_addr} {validator_addr} {moniker}\n")
 
-with open('gentx-output.txt', 'a') as outfile:
+with open('gentx-output.csv', 'a') as outfile:
     outfile.write(invalids)
 
-print('Written to file gentx-output.txt')
+print('Written to file gentx-output.csv')
